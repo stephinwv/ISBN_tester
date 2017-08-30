6906a6460e0ffdd472508ba10c330661ed5f4553
stephinwv <thekingdomseeker@aol.com>
2017-08-30 15:36:31 -0400

    renamed a few things, got tests passing so I thought was a good time to push

--- isbn_function.rb
+++ isbn_function.rb
@@ -1,19 +1,19 @@
 
 def isbn_remove(isbn_num)
-    clean_isbn = isbn_num.gsub!(/[-, ]/,"")
+    isbn_num.gsub!(/[-, ]/,"")
     # [^0-9A-Za-z] saw someone else use this but I'm not sure yet what it means.
-
+    isbn_num
 end
-def isbn_length10(isbn_num)
-     clean_isbn.length == 10
+def isbn_length(isbn_num)
+     if isbn_num.length == 10
     	true
-    	elsif clean_isbn.length == 13
+    	elsif isbn_num.length == 13
     	true
     	else
     	false
     end	
 end
-def check_sum(isbn_num)
+def check_num(isbn_num)
 	array = []
 	dig = ["0","1","2","3","4","5","6","7","8","9","x"]
 	isbn_num.each do |v|
