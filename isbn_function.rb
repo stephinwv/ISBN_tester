725a35c3ff50c8cbf272d254f02afea4716058a0
stephinwv <thekingdomseeker@aol.com>
2017-08-30 15:22:36 -0400

    Combined functions to check length, added checknum function to check for numbers and x. not sure if it works yet, just pushing what I have.

--- isbn_function.rb
+++ isbn_function.rb
@@ -1,15 +1,26 @@
+
 def isbn_remove(isbn_num)
-    isbn_num.gsub(/[-, ]/,"")
+    clean_isbn = isbn_num.gsub!(/[-, ]/,"")
+    # [^0-9A-Za-z] saw someone else use this but I'm not sure yet what it means.
+
 end
 def isbn_length10(isbn_num)
-     isbn_num.length == 10
-    # 	isbn_num = true
-    # 	elsif isbn_num.length == 13
-    # 	isbn_num = true
-    # 	else
-    # 	isbn_num = false
-    # end	
+     clean_isbn.length == 10
+    	true
+    	elsif clean_isbn.length == 13
+    	true
+    	else
+    	false
+    end	
 end
-def isbn_length13(isbn_num)
-	isbn_num.length == 13
+def check_sum(isbn_num)
+	array = []
+	dig = ["0","1","2","3","4","5","6","7","8","9","x"]
+	isbn_num.each do |v|
+		if dig.include?(v)
+			isbn_num == true
+			else
+				isbn_num == false
+		end
+	end	
 end
